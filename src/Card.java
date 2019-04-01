82b696e2c9bc5c3cefecf84c85971d1c21bae5ee
Nathan <nathan@Ds-MacBook-Air.local>
2019-04-01 08:17:51 -0600

    part1 upload

--- src/Card.java
+++ src/Card.java
@@ -33,6 +33,9 @@ public class Card {
 	 */
 	public Card(String cardRank, String cardSuit, int cardPointValue) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		this.rank = cardRank;
+		this.suit = cardSuit;
+		this.pointValue = cardPointValue;
 	}
 
 
@@ -42,6 +45,7 @@ public class Card {
 	 */
 	public String suit() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return this.suit;
    }
 
 	/**
@@ -50,6 +54,7 @@ public class Card {
 	 */
 	public String rank() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return this.rank;
 	}
 
    /**
@@ -58,6 +63,7 @@ public class Card {
 	 */
 	public int pointValue() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return this.pointValue;
 	}
 
 	/** Compare this card with the argument.
@@ -68,6 +74,9 @@ public class Card {
 	 */
 	public boolean matches(Card otherCard) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return this.suit().equals(otherCard.suit()) && this.rank().equals(otherCard.rank()) && this.pointValue() == otherCard.pointValue();
+			
+	
 	}
 
 	/**
@@ -83,5 +92,6 @@ public class Card {
 	@Override
 	public String toString() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return this.rank() + " of " + this.suit() + "(poinValue = " + this.pointValue() + ")";
 	}
 }
--- src/CardTester.java
+++ src/CardTester.java
@@ -9,5 +9,10 @@ public class CardTester {
 	 */
 	public static void main(String[] args) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card card1 = new Card("ace", "spades",10);
+		Card card2 = new Card("ace", "spades", 10);
+		Card card3 = new Card("queen", "diamond", 10);
+		System.out.println(card1.matches(card2));
+		System.out.println(card3.toString());
 	}
 }
